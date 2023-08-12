307257ce147da5fe3ebbb5d169459c5fcb41c361
nick <nikoladarkangel@gmail.com>
2023-08-12 23:25:58 +0300

    finish

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-APPS_DIR = BASE_DIR.joinpath("apps")
+APPS_DIR = BASE_DIR.joinpath("applications")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
